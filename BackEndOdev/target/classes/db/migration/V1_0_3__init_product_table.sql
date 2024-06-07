6b450326ed537ebb02a36e6b1fbbf579a98a9996
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-06-07 15:02:45 +0300

    Delete BackEndOdev/target/classes/db/migration/V1_0_3__init_product_table.sql

--- BackEndOdev/target/classes/db/migration/V1_0_3__init_product_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE product (
-  `id` BINARY(16) NOT NULL,
-  `product_name` VARCHAR(45) NOT NULL,
-  `product_quantity` INT NOT NULL,
-  `product_description` VARCHAR(300) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
