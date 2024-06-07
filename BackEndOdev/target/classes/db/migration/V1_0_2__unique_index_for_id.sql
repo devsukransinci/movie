4aae346ffbdaaf64c0b0e89104cd1a0c11d101d5
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-06-07 15:02:51 +0300

    Delete BackEndOdev/target/classes/db/migration/V1_0_2__unique_index_for_id.sql

--- BackEndOdev/target/classes/db/migration/V1_0_2__unique_index_for_id.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE employee;
-CREATE TABLE employee (
-  `id` BINARY(16) NOT NULL,
-  `employee_name` VARCHAR(45) NOT NULL,
-  `employee_password` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
